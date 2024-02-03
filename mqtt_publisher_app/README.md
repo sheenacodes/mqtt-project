89733a9ac8452c00882fbd7f4f5a9369e9737104
Sheena <11192150+sheenacodes@users.noreply.github.com>
2024-02-03 14:14:10 +0200

    mqtt publisher dockerised

---
+++ mqtt_publisher_app/README.md
@@ -0,0 +1,6 @@
+to publish to mqtt
+```bash
+cd mqtt_publisher_app
+poetry install
+poetry run publish
+```
--- mqtt_publisher_app/mqtt_publisher_app/publish.py
+++ mqtt_publisher_app/mqtt_publisher_app/publish.py
@@ -1,11 +1,25 @@
 import time
 import paho.mqtt.client as mqtt
 import os
+import logging
+import random
+import json
+
+# set logging level
+logging.basicConfig(level=logging.INFO)
 
 # MQTT broker details
 broker_address = os.getenv("MQTT_BROKER_ADDR", "localhost")
-port = os.getenv("MQTT_BROKER_PORT", 1883)
-topic = os.getenv("MQTT_TOPIC", "iot_1")
+port = int(os.getenv("MQTT_BROKER_PORT", 1883))
+MQTT_TOPIC_PREFIX = "charger/1/connector/1/session/"
+
+
+def on_connect(client, userdata, flags, rc):
+    logging.info(f"Connected with result code {rc}")
+
+
+def on_publish(client, userdata, mid):
+    logging.info(f"Message {mid} published")
 
 
 def main():
@@ -15,11 +29,19 @@ def main():
     client.loop_start()
 
     try:
+        session_id = 1
         while True:
-            message = "Hello, MQTT!"
-            client.publish(topic, message)
-            print(f"Message published: {message}")
-            time.sleep(5)
+            message = {
+                "session_id": session_id,
+                "energy_delivered_in_kWh": random.randint(1, 70),
+                "duration_in_seconds": random.randint(10, 100),
+                "session_cost_in_cents": random.randint(30, 100),
+            }
+            topic = f"{MQTT_TOPIC_PREFIX}{session_id}"
+            client.publish(topic, json.dumps(message))
+            logging.info(f"Message published: {message}")
+            time.sleep(60)
+            session_id += 1
 
     except KeyboardInterrupt:
         print("Exiting...")
