f038a8197895d2bc5ece58164b0d6eee7ccd51ad
Sheena <11192150+sheenacodes@users.noreply.github.com>
2024-02-03 12:40:02 +0200

    saves incoming messages to mongo db

--- mqtt_subscriber_app/mqtt_subscriber_app/subscribe.py
+++ mqtt_subscriber_app/mqtt_subscriber_app/subscribe.py
@@ -1,10 +1,21 @@
 import paho.mqtt.client as mqtt
 import os
+from pymongo import MongoClient
 
 # MQTT broker details
 broker_address = os.getenv("MQTT_BROKER_ADDR", "localhost")
 port = os.getenv("MQTT_BROKER_PORT", 1883)
 topic = os.getenv("MQTT_TOPIC", "iot_1")
+
+# MongoDB Configuration
+mongo_uri = os.getenv("MONGO_URI", "mongodb://admin:password@localhost:27017/")
+database_name = os.getenv("MONDO_DB_NAME", "mqtt_messages")
+collection_name = os.getenv("MONGO_COLLECTION_NAME", "messages")
+
+# MongoDB Connection
+mongo_client = MongoClient(mongo_uri)
+db = mongo_client[database_name]
+collection = db[collection_name]
 
 
 def on_connect(client, userdata, flags, rc):
@@ -12,8 +23,19 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(topic)
 
 
-def on_message(client, userdata, msg):
-    print(f"Received message: {msg.payload.decode()}")
+def on_message(client, userdata, message):
+    # Callback function when a new message is received
+    payload = message.payload.decode("utf-8")
+    print(f"Received message: {payload}")
+
+    # Save the message to MongoDB
+    save_to_mongo(payload)
+
+
+def save_to_mongo(message):
+    # Insert the message into the MongoDB collection
+    collection.insert_one({"message": message})
+    print("Message saved to MongoDB")
 
 
 def main():
@@ -33,6 +55,8 @@ def main():
 
     except KeyboardInterrupt:
         print("Exiting...")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
     finally:
         client.disconnect()
